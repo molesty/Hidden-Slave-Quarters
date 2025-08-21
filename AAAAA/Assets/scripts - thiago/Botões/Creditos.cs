401af4aa0134ab618854f6453d66e4406d146882
molesty <thiago.matos1@alunos.sc.senac.br>
2025-08-21 09:30:47 -0300

    botao de creditos
    creditos

---
+++ AAAAA/Assets/scripts - thiago/Botões/Creditos.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+}
+
